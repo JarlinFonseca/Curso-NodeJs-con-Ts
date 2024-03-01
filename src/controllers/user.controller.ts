9d2ac17ef6cf89c3318e478a876af6e2d42adf44
Jarlin <jarlinandres5000@gmail.com>
2024-03-01 14:38:56 -0500

    Clase 2 -Configurando el arbol de rutas, las rutas y los controladores

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,12 @@
+﻿import {Request, Response} from "express";
+
+export class UserController {
+
+    getUsers(req: Request, res: Response){
+        res.status(200).json({
+            user: "Jarlin Fonseca",
+        });
+
+    }
+
+}
---
+++ src/router/router.ts
@@ -0,0 +1,19 @@
+﻿import { Router } from "express";
+
+export class BaseRouter<T> {
+
+    public router: Router;
+    public controller: T;
+    //public middleware: U
+
+    constructor(TController: {new (): T}){
+        this.router = Router();
+        this.controller = new TController();
+        this.routes();
+    }
+
+    routes(){
+
+    }
+
+}
