2b1af56e049e987a2f27a4c551254570f4c0685f
Jarlin <jarlinandres5000@gmail.com>
2024-03-04 09:40:08 -0500

    Clase 5 -Se crearon las entidades con sus relaciones usando TypeORM

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,21 +1,34 @@
-﻿import { Column, Entity } from "typeorm";
+﻿import { Column, Entity, OneToOne } from "typeorm";
 import { BaseEntity } from "../../config/base.entity";
+import { CustomerEntity } from "../../customer/entities/customer.entity";
 
 
 @Entity({ name: "user" })
 export class UserEntity extends BaseEntity {
   @Column()
-  username!: string;
-
-  @Column({ length: 1000 })
   name!: string;
 
   @Column()
   lastname!: string;
+
+  @Column()
+  username!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  password!: string;
 
   @Column({ nullable: true })
   jobPosition?: string;
 
   @Column()
   numberPhone!: number;
+
+  @Column({nullable: true})
+  streetAddress?: string;
+
+  @OneToOne(()=>CustomerEntity, (customer)=> customer.user)
+  customer!: CustomerEntity;
 }
